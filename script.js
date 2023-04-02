68598bd49db9cfd055596f249f7281b767ddf1ff
hambarcumyannarek <narmenia878@gmail.com>
2023-04-02 14:09:39 +0400

    new version

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const button = document.querySelector(".menuBtn");
 const menu = document.querySelector(".hamburger-menu");
 button.addEventListener("click",() => {
     menu.classList.add("active");
+    document.body.style.overflowY = 'hidden';
 })
 
 const closeBtn = document.querySelector('.closeBtn');
 
 closeBtn.addEventListener("click",() => {
+    document.body.style.overflowY = 'auto';
     menu.classList.remove("active");
 })
